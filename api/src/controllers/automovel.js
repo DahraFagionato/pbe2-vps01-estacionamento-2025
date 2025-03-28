a4fee0d97aa57b8685c7a38015a5358cb1b857e9
DahraFagionato <dahra.fagionato@gmail.com>
2025-03-28 13:27:11 -0300

    pbe

---
+++ api/src/controllers/automovel.js
@@ -0,0 +1,80 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const create = async (req, res) => {
+    try {
+        const automovel = await prisma.automovel.create({
+            data: req.body
+        });
+        res.status(201).json(automovel).end();
+    } catch (e) {
+        res.status(400).json({ error: e.message }).end();
+    }
+};
+
+const read = async (req, res) => {
+    try {
+        const automoveis = await prisma.automovel.findMany({
+            include: {
+                estadias: true
+            }
+        });
+        res.json(automoveis);
+    } catch (e) {
+        res.status(400).json({ error: e.message }).end();
+    }
+};
+
+const readOne = async (req, res) => {
+    try {
+        const automovel = await prisma.automovel.findUnique({
+            where: {
+                placa: req.params.placa
+            },
+            include: {
+                estadias: true
+            }
+        });
+        if (!automovel) {
+            return res.status(404).json({ error: "Automóvel não encontrado" }).end();
+        }
+        res.json(automovel);
+    } catch (e) {
+        res.status(400).json({ error: e.message }).end();
+    }
+};
+
+const update = async (req, res) => {
+    try {
+        const automovel = await prisma.automovel.update({
+            data: req.body,
+            where: {
+                placa: req.params.placa
+            }
+        });
+        res.status(202).json(automovel).end();
+    } catch (e) {
+        res.status(400).json({ error: e.message }).end();
+    }
+};
+
+const remove = async (req, res) => {
+    try {
+        const automovel = await prisma.automovel.delete({
+            where: {
+                placa: req.params.placa
+            }
+        });
+        res.status(204).end();
+    } catch (e) {
+        res.status(400).json({ error: e.message }).end();
+    }
+};
+
+module.exports = {
+    create,
+    read,
+    readOne,
+    update,
+    remove
+};
